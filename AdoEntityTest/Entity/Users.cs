6c2eed4a3d90c0168e12e6dd726f977055a97796
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2022-08-06 04:52:18 +0530

    code update

---
+++ AdoEntityTest/Entity/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ado.Entity;
+
+namespace AdoEntityTest.Entity
+{
+    class Users:AdoBase
+    {
+        [Primary]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public double Salary { get; set; }
+        public int Age { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
---
+++ AdoEntityTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoEntityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbConnection con = new DbConnection();
+            var result=con.AddUser();
+            Console.WriteLine($"DB connection Done {result}");
+            Console.Read();
+        }
+    }
+}
